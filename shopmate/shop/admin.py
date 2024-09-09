92f895efae3f6578978a0fb2b718a84b3f7838b2
Sambhav Acharya <work.sambhavacharya@gmail.com>
2024-09-09 16:18:01 +0545

    defined shop app in settings and registered the models in admin

--- shopmate/shop/admin.py
+++ shopmate/shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Product, ProductVariant, Inventory, Category
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductVariant)
+admin.site.register(Inventory)
+admin.site.register(Category)
