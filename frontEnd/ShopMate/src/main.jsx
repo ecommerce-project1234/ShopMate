ef547ab429dde125ee1ba3352976efbda42ad752
Suraj Telrandhe <suraj.telrandhe16@gmail.com>
2024-09-09 13:46:25 +0530

    added folders

--- frontEnd/ShopMate/src/main.jsx
+++ frontEnd/ShopMate/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }, {
+    path: "/about",
+    element: <div>About</div>,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
