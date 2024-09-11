9d7a284dcacbcc37b58e10eda6c77f882286e0a2
Aman Saharan <amansaharan019@gmail.com>
2024-09-11 05:44:43 +0530

    Add a test file for render product from models so that we can test. Also create a urls file for shop app.

--- shopmate/shop/views.py
+++ shopmate/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def test(request):
+    products = Product.objects.all()
+    return render(request, 'test.html', {'products':products})
