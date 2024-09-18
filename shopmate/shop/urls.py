9088b52613c10ba78b839cd46f26d41e8688fccb
Sambhav Acharya <work.sambhavacharya@gmail.com>
2024-09-18 23:05:17 +0545

    added product detail and a product

--- shopmate/shop/urls.py
+++ shopmate/shop/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     
-    path("", views.test, name="test")
+    path("", views.test, name="test"),
     
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart')
 ]
